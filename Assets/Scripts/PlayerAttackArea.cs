bfe063c1ebb93c03cf30e77a0606adcebea4fa7b
honghak19 <hhg121105@gmail.com>
2025-07-27 18:28:10 -0700

    lan dau day du lieu

---
+++ Assets/Scripts/PlayerAttackArea.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerAttackArea : MonoBehaviour
+{
+    public int damage = 10;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+                Debug.Log($"✅ Chém trúng {other.name}!");
+            }
+        }
+    }
+}
